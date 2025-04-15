841b9e3f46ddfc2b24bf4c19c04078cdf035887f
Sirion Taiyasakda <sirion_taiyasakda@sluz.ch>
2025-04-15 14:59:14 +0200

    add props

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,16 @@ import Tweet from "../components/icons/Tweet.vue";
 </script>
 
 <template>
-    <LoginInfo />
-    <Composer />
+  <LoginInfo />
+  <Composer />
 
-    <!-- Stream -->
-    <section class="stream">
-      <Tweet v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :tweet="tweet" />
-    </section>
+  <!-- Stream -->
+  <section class="stream">
+    <Tweet
+      v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
+      :user="{ id: 1, name: 'John Doe' }"
+      :text="'Lorem ipsum dolor med'"
+      :createdAt="'2023-01-01T12:00:00Z'"
+    />
+  </section>
 </template>
