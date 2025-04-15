2be83e74055a564d37e85f2cc28370f7d220de80
Sirion Taiyasakda <sirion_taiyasakda@sluz.ch>
2025-04-15 15:25:37 +0200

    add loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,24 @@
 import LoginInfo from "../components/LoginInfo.vue";
 import Composer from "../components/Composer.vue";
 import Tweet from "../components/Tweet.vue";
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+import { fetchStream } from '../api/requests'
+
+const loading = ref(true)
+
+onMounted(async () => {
+  loading.value = true
+    try {
+        const stream = await fetchStream()
+        
+        console.log('Stream geladen', stream)
+    } catch (error) {
+        console.error(error)
+    } finally {
+        loading.value = false
+    }
+})
 </script>
 
 <template>
@@ -9,7 +27,7 @@ import Tweet from "../components/Tweet.vue";
   <Composer />
 
   <!-- Stream -->
-  <section class="stream">
+  <section class="stream" v-if="!loading">
     <Tweet
       v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
       :user="{ id: 1, name: 'John Doe' }"
@@ -17,4 +35,8 @@ import Tweet from "../components/Tweet.vue";
       :createdAt="'2023-01-01T12:00:00Z'"
     />
   </section>
+
+  <div class="loading">
+    Lade Tweets...
+  </div>
 </template>
