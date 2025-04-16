ab32fa2d4a7c2234717089e0dc29997c31719b8a
Sirion Taiyasakda <sirion_taiyasakda@sluz.ch>
2025-04-16 10:58:24 +0200

    add sendTweet

--- src/components/Composer.vue
+++ src/components/Composer.vue
@@ -1,28 +1,38 @@
 <script setup>
 import { ref } from "vue"
 import { useAuth } from '../api/auth'
+import { createTweet } from './../api/requests'
 
 const { isLoggedIn } = useAuth()
 const tweetText = ref('')
+
+function sendTweet() {
+  console.log("Tweet senden:", tweetText.value)
+  createTweet(tweetText.value)
+  tweetText.value = ''
+}
 </script>
+
 
 <template>
   <!-- Composer -->
-  <form class="composer" v-if="isLoggedIn">
+  <form class="composer" v-if="isLoggedIn" @submit.prevent="sendTweet">
     <label class="composer__prompt">Was geht?</label>
     <textarea
       maxlength="160"
       class="composer__textarea"
       placeholder="Verfasse einen Tweet..."
-      v-model="tweetText"/>
+      v-model="tweetText" />
     <div class="composer__actions">
       <div class="composer__stats stats">
         <span class="stats__counter">{{ tweetText.length }}</span>
         <span class="stats__max">/ 160</span>
       </div>
-      <button :disabled="tweetText.length < 5" class="btn btn--primary">
-        Tweet veröffentlichen
-      </button>
+      <span @submit.prevent="handleSubmit">
+        <button class="btn btn--primary" type="submit" :disabled="tweetText.length < 5">
+          Tweet veröffentlichen
+        </button>
+      </span>
     </div>
   </form>
 </template>
