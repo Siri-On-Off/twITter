ff838c0d24bffaeafa8346781092870124ad4e36
Sirion Taiyasakda <sirion_taiyasakda@sluz.ch>
2025-04-16 14:29:24 +0200

    format timestamp

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,13 +1,28 @@
 <script setup>
-defineProps({
-  user: Object,
-  text: String,
-  createdAt: String,
+import { format, parseISO } from "date-fns";
+import { computed } from "vue";
+
+const props = defineProps({
+  user: {
+    type: Object,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    required: true,
+  },
 });
+
+const formattedDate = computed(() =>
+  format(parseISO(props.createdAt), "dd.MM.yyyy HH:mm")
+);
 </script>
 
 <template>
-  <!-- Tweet -->
   <div class="tweet">
     <div class="tweet__avatar">
       <img :src="`https://i.pravatar.cc/100?u=${user.id}`" alt="" />
@@ -15,7 +30,7 @@ defineProps({
     <div class="tweet__content">
       <div class="tweet__header">
         <span class="tweet__author">{{ user.name }}</span>
-        <span class="tweet__timestamp">{{ createdAt }}</span>
+        <span class="tweet__timestamp">{{ formattedDate }}</span>
       </div>
       <div class="tweet__text">
         {{ text }}
