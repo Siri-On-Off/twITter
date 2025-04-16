6959c8c0c475c7451389777ff361bc2d3397e243
Sirion Taiyasakda <sirion_taiyasakda@sluz.ch>
2025-04-16 10:41:28 +0200

    add logout handling

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,6 +2,16 @@
 import { RouterLink } from 'vue-router'  
 import IconLogin from './icons/IconLogin.vue'
 import IconHome from './icons/IconHome.vue'
+import { computed } from 'vue';
+import { useAuth } from '../api/auth'
+
+// "logout" neu auch importieren.
+const { isLoggedIn, logout } = useAuth()
+
+// Logout-Funktion beim Klick ausführen
+const handleLogout = () => {
+  logout()  // ← führt Logout durch (und sollte isLoggedIn aktualisieren)
+}
 </script>
 
 <template>
@@ -10,9 +20,13 @@ import IconHome from './icons/IconHome.vue'
       <IconHome />
       Stream
     </RouterLink>
-    <RouterLink to="/login">
+    <RouterLink to="/login" v-if="!isLoggedIn" >
       <IconLogin />
       Login
     </RouterLink>
+    <a href="#" class="logout-link" v-else @click.prevent="handleLogout">
+      <IconLogin />
+      Logout
+    </a>
   </nav>
 </template>
