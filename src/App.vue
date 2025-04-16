f07e35c4cde6e190cb9b32bc051fd73644b16701
Sirion Taiyasakda <sirion_taiyasakda@sluz.ch>
2025-04-16 15:04:08 +0200

    create link from twitter logo to home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
 import Logo from "./components/Logo.vue";
 import Navigation from "./components/Navigation.vue";
-import HomeViews from "./views/HomeView.vue";
 import Widget from "./components/Widget.vue";
 import TopUserWidget from "./components/TopUserWidget.vue";
 import NewUserWidget from "./components/NewUserWidget.vue";
@@ -379,19 +377,4 @@ a {
   margin-right: 30px;
 }
 
-.logo {
-  font-size: 2.6rem;
-  font-weight: 500;
-  padding: 20px 0;
-
-  background: linear-gradient(to bottom, #fb7185, #e11d48);
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.logo strong {
-  font-weight: 800;
-}
 </style>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,28 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
-  <!-- Logo -->
-  <div class="logo">
+  <RouterLink to="/" class="logo">
     Tw<strong>IT</strong>ter
-    </div>
+  </RouterLink>
 </template>
+
+<style>
+
+.logo {
+  font-size: 2.6rem;
+  font-weight: 500;
+  padding: 20px 0;
+
+  background: linear-gradient(to bottom, #fb7185, #e11d48);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.logo strong {
+  font-weight: 800;
+}
+</style>
